7b2196ed611e264ce7135216b985b7359d40c22a
Vaishal-Business <vaishal.business@gmail.com>
2026-08-20 14:20:20 +0400

    Refresh cached PWA scanner bundle

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='qrblast-v1';
+const CACHE='qrblast-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('/')))});
